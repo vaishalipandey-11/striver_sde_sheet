bca956ce1da07b1ab22b768f0f51cc3179ae233a
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2026-03-27 10:40:18 +0530

    day4

--- arrays3/Majority Element II(229).cpp
+++ arrays3/Majority Element II(229).cpp
@@ -44,3 +44,43 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 = 0, ele2 = 1; // initialize differently
+
+        // Step 1: Find candidates
+        for (int num : nums) {
+            if (num == ele1) cnt1++;
+            else if (num == ele2) cnt2++;
+            else if (cnt1 == 0) {
+                ele1 = num;
+                cnt1 = 1;
+            }
+            else if (cnt2 == 0) {
+                ele2 = num;
+                cnt2 = 1;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        // Step 2: Verify candidates
+        cnt1 = cnt2 = 0;
+        for (int num : nums) {
+            if (num == ele1) cnt1++;
+            else if (num == ele2) cnt2++;
+        }
+
+        vector<int> ans;
+        int n = nums.size();
+        if (cnt1 > n/3) ans.push_back(ele1);
+        if (cnt2 > n/3) ans.push_back(ele2);
+
+        return ans;
+    }
+};
