bff038343b50c35ebf657a6df4e7090df1883ba9
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2026-03-28 11:26:27 +0530

    day5

--- arrays3/Unique Paths(63).cpp
+++ arrays3/Unique Paths(63).cpp
@@ -19,3 +19,23 @@ public:
         
     }
 };
+
+// this is tabulation soln (bottom up)
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m, vector<int>(n,0));
+
+        //bc fill
+        for(int i=0; i<m ; i++) dp[i][0]=1;
+        for(int j=0; j<n ; j++) dp[0][j]=1;
+
+        // fill all dp
+        for(int i=1 ; i<m ; i++){
+            for(int j=1; j<n ; j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
