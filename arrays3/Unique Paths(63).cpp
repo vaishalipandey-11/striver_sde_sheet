4555fad2c1d0322947e3ee4d94e84fb283b71262
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2026-03-28 11:09:19 +0530

    day5

---
+++ arrays3/Unique Paths(63).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+// this is memo soln (top down )
+class Solution {
+public:
+    int solve(int i , int j , vector<vector<int>>&dp){
+       // bc
+        if(i==0 && j==0) return 1 ;
+        if(i<0 || j<0 ) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        return dp[i][j]= solve(i-1, j , dp)+ solve(i , j-1,dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m , vector<int>(n,-1));
+       return solve(m-1,n-1,dp); // o index soo m*n = m-1 n-1
+       
+        
+    }
+};
