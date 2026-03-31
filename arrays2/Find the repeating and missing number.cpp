81f36dc2651d2dfc48e675df92672cd30bafe82c
vaishalipandey-11 <pandeyvaishali002@gmail.com>
2026-03-31 15:39:51 +0530

    day8

--- arrays2/Find the repeating and missing number.cpp
+++ arrays2/Find the repeating and missing number.cpp
@@ -1,4 +1,4 @@
-// 1 loop count =2 (repeat) count =1 (missing   )
+// 1 loop count =2 (repeat) count = 0(missing   )
  //hash for repeat and missing
  //freq vector for repeat and missing indx marking trick
  
